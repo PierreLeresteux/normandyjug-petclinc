1b74ab6e1bee0e00554f9c218a57f909b5b43dbe
Pierre Leresteux <pierre.leresteux@gmail.com>
2016-01-15 14:33:05 +0100

    Change mvn path

--- execute.sh
+++ execute.sh
@@ -2,4 +2,4 @@ mkdir /petclinc
 cd /petclinc
 git clone https://github.com/spring-projects/spring-petclinic.git
 cd /petclinc/spring-petclinic
-/maven/apache-maven-3.3.9/bin/mvn tomcat7:run
+mvn tomcat7:run
